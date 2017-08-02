ab3372d9d8036ecd7806d1dbdeab0a34200c55b3
hulkhuo@lexinfintech.com <hulkhuo@lexinfintech.com>
2017-08-02 17:47:03 +0800

    modify

---
+++ src/main/java/practice/jvmgo/HelloWorld.java
@@ -0,0 +1,10 @@
+package practice.jvmgo;
+
+/**
+ * Created by Hulk on 2017/8/2.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
